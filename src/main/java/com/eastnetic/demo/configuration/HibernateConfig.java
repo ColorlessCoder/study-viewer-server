8552bb8a642da3c3097a92d30ccca54637927ef2
Shakil <shakil.ruet.12@gmail.com>
2021-06-14 13:37:57 +0600

    Use ResourceLoader instead of util

--- src/main/java/com/eastnetic/demo/configuration/HibernateConfig.java
+++ src/main/java/com/eastnetic/demo/configuration/HibernateConfig.java
@@ -1,21 +1,23 @@
 package com.eastnetic.demo.configuration;
 
 import org.hibernate.SessionFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.util.ResourceUtils;
-
-import java.io.FileInputStream;
+import org.springframework.core.io.ResourceLoader;
 import java.io.IOException;
 import java.util.Properties;
 
 @Configuration
 public class HibernateConfig {
 
+    @Autowired
+    ResourceLoader resourceLoader;
+
     @Bean
     public SessionFactory sessionFactory() throws IOException {
         java.util.Properties applicationProperties = new Properties();
-        applicationProperties.load(new FileInputStream(ResourceUtils.getFile("classpath:application.properties")));
+        applicationProperties.load(resourceLoader.getResource("classpath:application.properties").getInputStream());
         Properties hbmProperties = new Properties();
         hbmProperties.setProperty("hibernate.connection.url", applicationProperties.getProperty("spring.datasource.url"));
         hbmProperties.setProperty("hibernate.connection.username", applicationProperties.getProperty("spring.datasource.username"));
